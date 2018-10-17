8cfad8ebabb9f835f196093c9840c7e1b1caf835
luoxiang <luoxiang@sinovatech.com>
2018-10-17 14:21:10 +0800

    update

---
+++ springboot-redis/src/test/java/com/example/demo/Test.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+/**
+ *
+ */
+public class Test {
+
+}
---
+++ springboot-utils/src/main/java/com/example/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.entity;
+
+/**
+ *
+ */
+public class User {
+
+    private String mobilePhone;
+
+    private String phone;
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
